731c8363a66137e460853ee722373b9bf18be10a
Eduard Kosenko <edik.kosenko@gmail.com>
2026-02-22 16:02:22 -0500

    implement update and delete mutators

--- resolvers/employeeResolvers.js
+++ resolvers/employeeResolvers.js
@@ -29,6 +29,36 @@ const employeeResolvers = {
         });
         return employee;
     },
+    update: async (
+      _,
+      {
+        id,
+        first_name,
+        last_name,
+        email,
+        gender,
+        designation,
+        salary,
+        date_of_joining,
+        department,
+      },
+    ) => {
+      const employee = await Employee.findByIdAndUpdate(id, {
+        first_name,
+        last_name,
+        email,
+        gender,
+        designation,
+        salary,
+        date_of_joining,
+        department
+      }, { new: true });
+      return employee;
+    },
+    delete: async (_, { id }) => {
+      const result = await Employee.findByIdAndDelete(id);
+      return !!result;
+    }
   },
   Query: {
     employees: async () => await Employee.find(),
